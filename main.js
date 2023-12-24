5e8083499ebcf6cad87bc68f29d6bba331fa5157
JER-OFC <138082354+JER-OFC@users.noreply.github.com>
2023-12-24 21:04:36 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const jer = require('./index.js')
+
+module.exports.igs = jer.igstory
+module.exports.igdl = jer.igdl
+module.exports.twitter = jer.twitter
+module.exports.tiktokStalk = jer.tiktokStalk
+module.exports.gempa = jer.gempa
